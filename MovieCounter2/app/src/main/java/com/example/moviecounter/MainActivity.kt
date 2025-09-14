f56cdf5faa5ccb7f4672324aa19533975f4b2739
KlinsmanFlores <klinsman1597@hotmail.com>
2025-09-14 17:59:55 -0500

    Laboratorio04 moviles fin

---
+++ MovieCounter2/app/src/main/java/com/example/moviecounter/MainActivity.kt
@@ -0,0 +1,102 @@
+package com.example.moviecounter
+
+import android.R.attr.text
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.moviecounter.ui.theme.MovieCounterTheme
+import androidx.compose.material3.Button
+import androidx.compose.ui.Alignment
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.getValue
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.saveable.rememberSaveable
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            PreviewProductCounter()
+        }
+    }
+}
+
+@Composable
+fun ProductCounter(modifier: Modifier = Modifier) {
+    var quantity by rememberSaveable { mutableStateOf(0) }
+    var comment by remember { mutableStateOf("") }
+
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Producto: Laptop Gaming")
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Text(text = "Cantidad: $quantity")
+        Spacer(modifier = Modifier.height(16.dp))
+
+        TextField(
+            value = comment,
+            onValueChange = { comment = it },
+            label = { Text("Comentario") }
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(onClick = { quantity++ }) {
+            Text("Agregar al carrito")
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewProductCounter() {
+    ProductCounter()
+}
+
+
+@Composable
+fun MovieCounter (modifier: Modifier = Modifier) {
+    var count by rememberSaveable { mutableStateOf(0) }
+    var movieName by rememberSaveable { mutableStateOf("") }
+    Column (
+        modifier = modifier.padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "you have added $count movies")
+        Spacer(modifier = Modifier.height(16.dp))
+        TextField(value = movieName, onValueChange = { movieName = it }, label = { Text("Movie name") })
+        Button(onClick = {
+            if (movieName.isNotEmpty()) {
+                count++
+                movieName= ""
+            }
+        }) {
+            Text(text = "Add movie")
+        }
+    }
+}
+
+@Composable
+fun PreviewMovieCounter() {
+    MovieCounter()
+}
